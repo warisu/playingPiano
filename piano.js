cfc20d4fee1c357ad55d0ea24e3dcb1e3512f66b
warisu <abdullahiabdulwaris@gmail.comgit config --global user.email abdullahiabdulwaris@gmail.com>
2023-02-06 03:28:58 +0100

    Add both click and key javascript functionality

--- piano.js
+++ piano.js
@@ -1,39 +1,59 @@
-// this is for laptop
-// window.addEventListener('keydown',  playSound);
-
-// function playSound(e){
-//     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-//     const  key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-//     if(!audio) return; // stop playing if key is not found
-//     audio.currentTime = 0;
-//     audio.play();
-//     key.classList.add('playing');
-// }
-
-// const keyPresses = document.querySelectorAll('.key');
-// for (let keyPress of keyPresses) {
-//     keyPress.addEventListener('transitionend', function removeTransition(e) {
-//         if(e.propertyName !== 'transform') return; // skip if does not transform
-//         this.classList.remove('playing');
-//     })
-// };
-// This is immediately invoked function expression
-(function (doc){
-    const pianoKeys = doc.querySelectorAll('.key');
-    let audio_files = [...doc.querySelectorAll('audio')]; // this enables you access the file
-    console.log(audio_files);
-    pianoKeys.forEach(key => {
-        key.addEventListener('click', (e) => {
-            playAudio(e.currentTarget.dataset.key) // i didn't use originalTarget because it only work in firefox
-        })
-    })
-
-    const playAudio = key => {
-        const audio_file = audio_files.find(e => e.getAttribute('data-key') == key);
-        console.log('audio_file src:', audio_file.src);
-        audio_file.play();
-        pianoKeys.classList.add('playing');
-    }
+function playSound(keyCode){
+    const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+    const  key = document.querySelector(`.key[data-key="${keyCode}"]`);
     
+    // stop playing if key is not found
+    if(!audio || !key) return; 
+    
+    audio.currentTime = 0;
+    audio.play();
+    
+    key.classList.add('playing');
+}
 
-})(document);
+function removeTransition(e) {
+    // skip if does not transform
+    if(e.propertyName !== 'transform') return; 
+    
+    this.classList.remove('playing');
+}
+
+const pianoKeys = document.querySelectorAll('.key');
+for (let pianoKey of pianoKeys) {
+   pianoKey.addEventListener('transitionend', removeTransition)
+}
+
+for (let pianoKey of pianoKeys) {
+    pianoKey.addEventListener('click', (e) => {
+        playSound(e.currentTarget.dataset.key)
+    })
+}
+
+window.addEventListener('keydown', (e) => {
+    playSound(e.keyCode)
+})
+
+// This is immediately invoked function expression
+// (function (doc){
+//     const pianoKeys = doc.querySelectorAll('.key');
+//     let audio_files = [...doc.querySelectorAll('audio')]; // this enables you access the file
+//     // console.log(audio_files);
+//     pianoKeys.forEach(key => {
+//         key.addEventListener('click', (e) => {
+//             playAudio(e.currentTarget.dataset.key) // i didn't use originalTarget because it only work in firefox
+//         })
+//     })
+
+//     const playAudio = key => {
+//             const audio_file = audio_files.find(e => e.getAttribute('data-key') == key);
+//             // console.log('audio_file src:', audio_file.src);
+//             audio_file.currentTime = 0;
+//             audio_file.play();
+//             addPlayingClass()
+//         } 
+    
+//     function addPlayingClass(e) {
+//         const currentKey = doc.querySelector(`.key=[data-key="${e.keyCode}"]`);
+//         currentKey.classList.add('playing')
+//     } 
+// })(document);
